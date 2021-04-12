2ac59cf21b5f1fae10f5dd7f81a20c32cf3a16c1
João Pedro Limão <jplimao12@gmail.com>
2021-04-12 19:14:50 -0300

    [server] feature: Create news models

--- server/news/admin.py
+++ server/news/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from news.models import News, UserNews, Comment
 
-# Register your models here.
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserNews)
+class UserNewsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- server/news/models.py
+++ server/news/models.py
@@ -1,3 +1,46 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=256)
+    subtitle = models.CharField(max_length=512)
+    text = models.TextField()
+    author = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
+
+    def __str__(self):
+        return self.title
+
+
+class UserNews(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    liked = models.BooleanField(default=False)
+    read = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'UserNews'
+        verbose_name_plural = 'UserNews'
+
+    def __str__(self):
+        return f'{self.user.first_name} - {self.news.title}'
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    def __str__(self):
+        return f'{self.user.first_name} - {self.news.title}'
+

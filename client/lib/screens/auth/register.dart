36630033e73aa7432b6c3f2491461b961a53f2c7
João Pedro Limão <jplimao12@gmail.com>
2021-04-13 14:04:57 -0300

    [client] study: Select TextField decoration

--- client/lib/screens/auth/register.dart
+++ client/lib/screens/auth/register.dart
@@ -10,20 +10,30 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(24.0),
         child: Column(
           children: [
             Text('Criar conta'),
             TextField(
-              decoration: InputDecoration(labelText: 'Nome'),
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.person),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                    borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                labelText: 'Nome',
+              ),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Email'),
             ),
             TextField(
+              obscureText: true,
               decoration: InputDecoration(labelText: 'Senha'),
             ),
             TextField(
+              obscureText: true,
               decoration: InputDecoration(labelText: 'Confirmação de senha'),
             ),
             ElevatedButton(

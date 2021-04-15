53677aaab85cdc56f9031e74c9723042f45c0572
João Pedro Limão <jplimao12@gmail.com>
2021-04-15 19:31:44 -0300

    [server] fix: Allow all hosts

--- server/core/settings.py
+++ server/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '95nlqg))jgr*tgl&&k=-i4e1in-d1haz$3-4!wx(mzzo$etfn$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 

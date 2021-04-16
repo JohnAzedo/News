47aa26b37a7696760133efea052011fbc349518b
João Pedro Limão <jplimao12@gmail.com>
2021-04-16 20:56:45 -0300

    [server] fix: Turn to "Allow Any" List news feature

--- server/news/views.py
+++ server/news/views.py
@@ -1,11 +1,11 @@
 from rest_framework.generics import ListCreateAPIView, ListAPIView, RetrieveAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from news.models import News
 from news.serializers import NewsSerializer
 
 
 class NewsListView(ListAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     serializer_class = NewsSerializer
     queryset = News.objects.all()
 

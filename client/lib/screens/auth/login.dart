b24ca73efb9d92aa54a86c5f99904e46f55b172b
Davi Rodrigues <davirodrigues8@gmail.com>
2021-04-27 21:21:54 -0300

    [client] fix: Add SafeArea to login screen

--- client/lib/screens/auth/login.dart
+++ client/lib/screens/auth/login.dart
@@ -19,13 +19,13 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  void _login(BuildContext context){
+  void _login(BuildContext context) {
     final User user = User(
       email: _emailController.text,
       password: _passwordController.text,
     );
 
-    if(user.email != '' && user.password != ''){
+    if (user.email != '' && user.password != '') {
       _getTokens(context, user);
     } else {
       showCheckNullValuesSnackBar(context);
@@ -48,40 +48,43 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text('Entrar'),
-            Text('Por favor, faça login para continuar.'),
-            CustomTextField(
-              controller: _emailController,
-              label: 'Email',
-              icon: Icon(Icons.email),
-            ),
-            CustomTextField(
-              controller: _passwordController,
-              isPassword: true,
-              label: 'Senha',
-              icon: Icon(Icons.lock),
-            ),
-            ElevatedButton(
-              onPressed: () => _login(context),
-              child: Text('Entrar'),
-            ),
-            Row(
-              children: [
-                Text('Não possui conta ainda? '),
-                RichText(
-                  text: TextSpan(
-                      style: TextStyle(color: Theme.of(context).primaryColor),
-                      text: 'Cadastre-se',
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () => navigateToInviteCode(context)),
-                )
-              ],
-            )
-          ],
+      body: SafeArea(
+        top: true,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Entrar'),
+              Text('Por favor, faça login para continuar.'),
+              CustomTextField(
+                controller: _emailController,
+                label: 'Email',
+                icon: Icon(Icons.email),
+              ),
+              CustomTextField(
+                controller: _passwordController,
+                isPassword: true,
+                label: 'Senha',
+                icon: Icon(Icons.lock),
+              ),
+              ElevatedButton(
+                onPressed: () => _login(context),
+                child: Text('Entrar'),
+              ),
+              Row(
+                children: [
+                  Text('Não possui conta ainda? '),
+                  RichText(
+                    text: TextSpan(
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                        text: 'Cadastre-se',
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () => navigateToInviteCode(context)),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );

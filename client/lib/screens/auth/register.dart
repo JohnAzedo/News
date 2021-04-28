1edb1a3c6e70e0f10d1514db7f62265e78cde003
Davi Rodrigues <davirodrigues8@gmail.com>
2021-04-28 20:00:46 -0300

    [client] fix: Add SafeArea to register screen

--- client/lib/screens/auth/register.dart
+++ client/lib/screens/auth/register.dart
@@ -13,56 +13,56 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
 
-  Future<void> _createUser() async{
+  Future<void> _createUser() async {
     final User user = User(
-      name: _nameController.text,
-      email: _emailController.text,
-      password: _passwordController.text,
-      confirmPassword: _passwordController.text
-    );
+        name: _nameController.text,
+        email: _emailController.text,
+        password: _passwordController.text,
+        confirmPassword: _passwordController.text);
 
-    return repository.register(user).then((value) => {
-      debugPrint('Here')
-    });
+    return repository.register(user).then((value) => {debugPrint('Here')});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          children: [
-            Text('Criar conta'),
-            CustomTextField(
-              controller: _nameController,
-              label: 'Nome',
-              icon: Icon(Icons.person),
-            ),
-            CustomTextField(
-              controller: _emailController,
-              label: 'Email',
-              icon: Icon(Icons.email),
-            ),
-            CustomTextField(
-              controller: _passwordController,
-              isPassword: true,
-              label: 'Senha',
-              icon: Icon(Icons.lock),
-            ),
-            CustomTextField(
-              controller: _confirmPasswordController,
-              isPassword: true,
-              label: 'Confirmar senha',
-              icon: Icon(Icons.lock),
-            ),
-            ElevatedButton(
-              onPressed: () => _createUser(),
-              child: Text('Criar'),
-            )
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              Text('Criar conta'),
+              CustomTextField(
+                controller: _nameController,
+                label: 'Nome',
+                icon: Icon(Icons.person),
+              ),
+              CustomTextField(
+                controller: _emailController,
+                label: 'Email',
+                icon: Icon(Icons.email),
+              ),
+              CustomTextField(
+                controller: _passwordController,
+                isPassword: true,
+                label: 'Senha',
+                icon: Icon(Icons.lock),
+              ),
+              CustomTextField(
+                controller: _confirmPasswordController,
+                isPassword: true,
+                label: 'Confirmar senha',
+                icon: Icon(Icons.lock),
+              ),
+              ElevatedButton(
+                onPressed: () => _createUser(),
+                child: Text('Criar'),
+              )
+            ],
+          ),
         ),
       ),
     );
